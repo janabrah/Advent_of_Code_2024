9d47fb63a7c657e17d506ed8af688cd3276f7e25
Jacob Abrahams <coby@MacBookAir.ht.home>
2024-12-01 14:40:53 -0600

    Adding a template for each day, I expect it to evolve over time

---
+++ template.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+f = open("input.txt")
+input = []
+for line in f:
+    input.append(line.strip("\n"))
+
+
+def prettyPrint(myArray):
+    for row in myArray:
+        print("".join([str(row[i]) for i in range(len(row))]))
+    print()
