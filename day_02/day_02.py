8188e655c3b30fef0683aa28652b458437eaf2aa
Jacob Abrahams <coby@MacBookAir.ht.home>
2024-12-02 09:21:50 -0600

    Day two quick first pass

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,12 +1,59 @@
-import numpy as np
+# import numpy as np
 
-f = open("input.txt")
+f = open("input_02.txt")
 input = []
 for line in f:
-    input.append(line.strip("\n"))
+    input.append([int(i) for i in line.strip("\n").split()])
 
 
 def prettyPrint(myArray):
     for row in myArray:
         print("".join([str(row[i]) for i in range(len(row))]))
     print()
+
+
+def checkRow(row, grace=False):
+    print(row)
+    growing = True
+    shrinking = True
+    if not grace and (checkRow(row[:-1], True) or checkRow(row[1:], True)):
+        return True
+    for i in range(len(row) - 1):
+        if row[i + 1] < row[i]:
+            if not shrinking:
+                print("not shrinking" + str(grace))
+                if not grace:
+                    return checkRow(row[:i] + row[i + 1 :], True) or checkRow(
+                        row[: i + 1] + row[i + 2 :], True
+                    )
+                return False
+            if growing and (not grace) and checkRow(row[:i] + row[i + 1 :], True):
+                return True
+            growing = False
+        elif row[i + 1] > row[i]:
+            if not growing:
+                print("not growing" + str(grace))
+                if not grace:
+                    return checkRow(row[:i] + row[i + 1 :], True) or checkRow(
+                        row[: i + 1] + row[i + 2 :], True
+                    )
+                return False
+            if shrinking and (not grace) and checkRow(row[:i] + row[i + 1 :], True):
+                return True
+            shrinking = False
+        if not (abs(row[i + 1] - row[i]) >= 1 and abs(row[i + 1] - row[i]) <= 3):
+            print("wrong spacing" + str(grace))
+            if not grace:
+                return checkRow(row[:i] + row[i + 1 :], True) or checkRow(
+                    row[: i + 1] + row[i + 2 :], True
+                )
+            return False
+    return True
+
+
+count = 0
+for row in input:
+    count += checkRow(row)
+print(count)
+
+# print(input[:5])
